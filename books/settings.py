2964d7ffd52062bd5b73afcd9eb1937d7ff83593
Artem Z <13085715+artem-zavgorodniy@users.noreply.github.com>
2021-10-17 13:33:15 +0300

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -9,12 +9,12 @@ ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
 
 DOWNLOADER_MIDDLEWARES = {
-'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610,
-'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    'scrapy_user_agents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+	'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610,
+	# 'scrapy_splash.SplashCookiesMiddleware': 723,
+ #    'scrapy_splash.SplashMiddleware': 725,
+ #    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+ #    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+ #    'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
     }
 ZYTE_SMARTPROXY_ENABLED = True
 ZYTE_SMARTPROXY_APIKEY = '05104e83eb2f4949a98ddfbbbeb90326'
