82eef103fe9d2ec53eff2cdadf40e65ef488cbf2
Artem Z <13085715+artem-zavgorodniy@users.noreply.github.com>
2021-10-17 14:05:05 +0300

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -8,7 +8,7 @@ NEWSPIDER_MODULE = 'books.spiders'
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
 
-DOWNLOADER_MIDDLEWARES = {'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610}
+DOWNLOADER_MIDDLEWARES = {'books.ZyteSmartProxyMiddleware': 610}
 ZYTE_SMARTPROXY_ENABLED = True
 ZYTE_SMARTPROXY_APIKEY = '05104e83eb2f4949a98ddfbbbeb90326'
 ZYTE_SMARTPROXY_URL = 'https://proxy.crawlera.com:8011'
